55701b8393e64dd3b80ea1d83784a4b9e60ea2f0
Domnic Dev Kulanthaisamy <domnic.k@itp.objectfrontier.com>
2024-01-17 19:21:23 +0530

    commit6

--- src/test/java/com/dms/SbDmsCicdApplicationTests.java
+++ src/test/java/com/dms/SbDmsCicdApplicationTests.java
@@ -1,9 +1,9 @@
 package com.dms;
 
-
 import static org.testng.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.dms.evaluation.model.Doctor;
@@ -11,9 +11,13 @@ import com.dms.evaluation.repository.DMSRepository;
 
 @SpringBootTest
 class SbDmsCicdApplicationTests {
-
+	@Autowired
 	static DMSRepository dao;
 	Doctor bean = new Doctor(102, "Domnic", "Heart", "12_01_24", "12:35", "Dev");
+
+	@Test
+	void contextLoads() {
+	}
 
 	@Test
 	public void testInsert() {
@@ -23,4 +27,3 @@ class SbDmsCicdApplicationTests {
 	}
 
 }
-
